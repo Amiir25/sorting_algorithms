cf145f67205abe9f2a55e23082e7f45957409223
Amiir25 <amirsadik636@gmail.com>
2024-10-26 11:59:08 +0300

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,9 +10,8 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp, swap;
+	int temp;
 
-	swap = 0;
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
@@ -22,13 +21,9 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swap++;
 
 				print_array(array, size);
 			}
 		}
-
-		if (swap == 0)
-			break;
 	}
 }
