fe17cfb89d8323b655e1d23a1b673a67d64f9a6c
Amiir25 <amirsadik636@gmail.com>
2024-10-27 11:03:04 +0300

    Add 2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an array using selection sort algorithm
+ *
+ * @array: The array
+ * @size: The size of @array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int smallest, idx;
+
+	for (i = 0; i < size; i++)
+	{
+		smallest = array[i];
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < smallest)
+			{
+				smallest = array[j];
+				idx = j;
+			}
+		}
+
+		array[idx] = array[i];
+		array[i] = smallest;
+		print_array(array, size);
+	}
+}
