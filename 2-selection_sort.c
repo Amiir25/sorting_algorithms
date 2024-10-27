a56905b8eb855684cab82a5669b2d63008c496d1
Amiir25 <amirsadik636@gmail.com>
2024-10-27 11:05:06 +0300

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,6 +12,9 @@ void selection_sort(int *array, size_t size)
 	size_t i, j;
 	int smallest, idx;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size; i++)
 	{
 		smallest = array[i];
